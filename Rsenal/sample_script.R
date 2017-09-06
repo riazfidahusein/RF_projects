d9efe7c8b1a27688b4a39b680f6e9cdaed478fb2
Riaz Fidahusein <riaz.fidahusein@gmail.com>
2017-09-06 20:41:08 +0000

    looking at simple w/l/d summary stats by season

--- Rsenal/sample_script.R
+++ Rsenal/sample_script.R
@@ -1,14 +1,31 @@
-library(devtools)
-install_github('jalapic/engsoccerdata', username = "jalapic")
+# code to install engsoccerdata package
+# library(devtools)
+# install_github('jalapic/engsoccerdata', username = "jalapic")
+
+
+library(dplyr)
 library(engsoccerdata)
 
-data(package="engsoccerdata")    # lists datasets currently available
-library(dplyr)
+# data(package="engsoccerdata")    # lists datasets currently available
 
 arse_games <- england %>% 
   filter(home == "Arsenal" | visitor == "Arsenal")
 
-summary(arse_games)
+# total games played
+games_played <- summarise(arse_games, n())
+
+# games won/ drawn/ lost
+season_home_wins <- arse_games %>% 
+  filter(home == "Arsenal" | result == 'h') %>% 
+  select(Season, home, visitor, hgoal, vgoal, totgoal) %>% 
+  group_by(Season)
+  summarise(n())
+
+away_wins <- arse_games %>% 
+  filter(home == "Arsenal" | result == 'h') %>% 
+  summarise(n())
+
+
 
 england %>% 
   filter(Season == min(Season)) %>% 
