e6dea18ddcb3ac328f0842846c7694265304bb1c
Riaz Fidahusein <riaz.fidahusein@gmail.com>
2017-09-12 20:48:36 +0000

    update to Rsenal script

--- Rsenal/sample_script.R
+++ Rsenal/sample_script.R
@@ -3,30 +3,63 @@
 # install_github('jalapic/engsoccerdata', username = "jalapic")
 
 
-library(dplyr)
+library(tidyverse)
+library(plyr)
 library(engsoccerdata)
 
 # data(package="engsoccerdata")    # lists datasets currently available
 
-arse_games <- england %>% 
-  filter(home == "Arsenal" | visitor == "Arsenal")
+teamName <- "Arsenal"
+
+tot_games <- england %>% 
+  filter(home == teamName | visitor == teamName)
 
 # total games played
-games_played <- summarise(arse_games, n())
+games_played <- summarise(tot_games, n())
 
 # games won/ drawn/ lost
-season_home_wins <- arse_games %>% 
-  filter(home == "Arsenal" | result == 'h') %>% 
-  select(Season, home, visitor, hgoal, vgoal, totgoal) %>% 
-  group_by(Season)
-  summarise(n())
+home_game_outcomes <- tot_games %>% 
+  filter(home == teamName) %>% 
+  select(result) %>%
+  mutate(outcome = replace(result, which(result == "A"), "L")) %>%
+  mutate(outcome = replace(outcome, which(result == "H"), "W")) %>%
+  select(outcome) %>% 
+  group_by(outcome) %>% 
+  tally()
+  
+away_game_outcomes <- tot_games %>% 
+  filter(visitor == teamName) %>% 
+  select(result) %>%
+  mutate(outcome = replace(result, which(result == "A"), "W")) %>%
+  mutate(outcome = replace(outcome, which(result == "H"), "L")) %>%
+  select(outcome) %>% 
+  group_by(outcome) %>% 
+  tally()
 
-away_wins <- arse_games %>% 
-  filter(home == "Arsenal" | result == 'h') %>% 
-  summarise(n())
+
+# build league table
+zz <- england %>% 
+  arrange(Date) %>% 
+  dlply("Season", identity)
 
 
+tmp <- zz$`1888`
+
+
+teams <- unique(c(tmp$home, tmp$visitor))
+
+
+
+build_league_table()
 
 england %>% 
-  filter(Season == min(Season)) %>% 
-  select(Date)
+  filter(Season == 1888) %>%
+  select(home, visitor) %>% 
+  
+
+
+
+
+
+
+
